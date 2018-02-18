e8aae095f1e1e02e766f9b737dca60933dac8393
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-02-18 13:42:31 -0800

    Rename ellipses-arrays.js to ellipses1-arrays.js

---
+++ ellipses1-arrays.js
@@ -0,0 +1,35 @@
+var x = [];
+var y = [];
+var d = [];
+var colors = [];
+
+
+function setup() {
+	createCanvas(400, 400);
+
+	for (var index = 0; index < 50; index = index + 1) {
+		x[index] = random(width);
+		y[index] = random(height);
+		d[index] = random(5, 20);
+		colors[index] = color(random(255), random(255), random(255))
+
+
+	}
+}
+
+function draw() {
+	background(0);
+	noStroke();
+
+	for (var index = 0; index < 50; index = index + 1) {
+		fill(colors[index]);
+		ellipse(x[index], y[index], d[index]);
+
+		// 1% of the time
+		if (random() < 0.01) {
+			// random diameter between 10 and 400
+			d[index] = random(10, 40);
+		}
+
+	}
+}
