5f9bccc86591955357f64ae9691111590a10d5cf
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-02-17 22:36:19 -0800

    Update ellipses-arrays.js

--- ellipses-arrays.js
+++ ellipses-arrays.js
@@ -1,40 +1,38 @@
 var x = [];
 var y = [];
-var xSpeed = [];
 var ySpeed = [];
 var colors = [];
 
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
 
-  for (var index = 0; index < 100; index = index + 1) {
-    x[index] = 230;
-    y[index] = 220;
-    xSpeed[index] = random(1, 5);
-    ySpeed[index] = random(1, 5);
-    colors[index] = color(random(255), random(255), random(255))
-  }
+	for (var index = 0; index < 20; index = index + 1) {
+		x[index] = 230;
+		y[index] = 220;
+
+		ySpeed[index] = 5;
+		colors[index] = color(random(255), random(255), random(255))
+	}
 }
-  
+
 function draw() {
-  background(0);
-  noStroke();
+	background(0);
+	noStroke();
 
-  // draw pipe
-  rect(0, 200, 220, 20);
-  
-  // draw drip
-  for (var index = 0; index < 100; index = index + 1) {
-    fill(colors[index]);
-    ellipse(x[index], y[index], 10);
-    x[index] = x[index] + xSpeed[index];
-    y[index] = y[index] + ySpeed[index];
+	// draw pipe
+	rect(0, 200, 220, 20);
 
-  
-  // down 3 pixels each frame, but maybe should be accelerating?
-  y = y + 3
-  
-  // if invisible for a full “height” amount…
- 
-}
+	// draw drip
+	for (var index = 0; index < 20; index = index + 1) {
+		fill(colors[index]);
+		ellipse(x[index],y[index],10);
+		y[index] = y[index] + ySpeed[index];
+
+
+		// down 3 pixels each frame, but maybe should be accelerating?
+		y[index] = y[index] + 1
+
+		// if invisible for a full “height” amount…
+
+	}
 }
